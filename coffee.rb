c31354728c0812547419c1fe7d166acc9d9fc569
Clyde Autin <clyde.autin@gmail.com>
2024-04-04 08:46:47 -0700

    Initial commit

---
+++ coffee.rb
@@ -0,0 +1,10 @@
+class Coffee
+
+    attr_reader :name, :origin_country
+
+    def initialize(name, origin_country)
+        @name = name
+        @origin_country = origin_country
+    end
+    
+end
